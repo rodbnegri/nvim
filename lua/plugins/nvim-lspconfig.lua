7a5e9a0a0d4c357121ea3e36fbc654a36cfb5ecf
Rodolfo B. Negri <rodolfobnegri@yahoo.com.br>
2024-03-21 10:40:09 -0300

    tailored clangd LSP setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,24 @@ return {
   opts = {
     servers = {
       julials = {},
-      clangd = {},
+      clangd = {
+        cmd = { "clangd", "--clang-tidy", "--completion-style=detailed", "--header-insertion=never", "--enable-config" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        settings = {
+          cquery = {
+            cache = {
+              directory = "/tmp/cquery-cache",
+            },
+            completion = {
+              autoComplete = true,
+            },
+            diagnostics = {
+              enable = true,
+              extraArgs = { "-isystem", "/usr/include/c++" },
+            },
+          },
+        },
+      },
       fortls = {},
     },
   },
