71d13a8b415e6cc7fecd4d7ae03470032a841ffb
Rodolfo B. Negri <rodolfobnegri@yahoo.com.br>
2024-03-27 13:53:09 -0300

    change colorscheme to onedarkpro

--- init.lua
+++ init.lua
@@ -1,8 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim, and your plugins
 require("config.lazy")
 
-vim.cmd("colorscheme nordic")
 
--- Customize the color of comments
-vim.cmd('highlight Comment guifg=#006461')
+
+
 
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -3,12 +3,5 @@ return {
   "olimorris/onedarkpro.nvim",
   priority = 10, -- Ensure it loads first
 },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("nordic").load()
-    end,
-  },
 }
+
