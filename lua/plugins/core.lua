9271a7e9c1293304f79bd6100abcefff400b2730
Rodolfo B. Negri <rodolfobnegri@yahoo.com.br>
2024-03-28 11:19:53 -0300

    + TODO highlights added (todo.lua)

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,7 +1,15 @@
 return {
-{
-  "olimorris/onedarkpro.nvim",
-  priority = 10, -- Ensure it loads first
-},
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 10, -- Ensure it loads first
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
-
