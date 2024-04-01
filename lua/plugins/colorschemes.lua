efab18f4ab821ea91f84fa5a0d3ac9484e46155e
Rodolfo B. Negri <rodolfobnegri@yahoo.com.br>
2024-04-01 11:15:04 -0300

    set up vimtex

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,19 @@
 -- Lazy
 return {
-  --   { "rebelot/kanagawa.nvim", priority = 1000 },
+  --   --   { "rebelot/kanagawa.nvim", priority = 1000 },
   { "Mofiqul/vscode.nvim", priority = 1000 },
+  --
 }
+--
+-- return {
+--   { "catppuccin/nvim" },
+--   lazy = false,
+--   name = "catppuccin",
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
