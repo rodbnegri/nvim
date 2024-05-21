871aa2aeb6c2af137fd02c52556663dbad99023b
rodolfo <rodolfobnegri@yahoo.com.br>
2024-05-21 16:08:43 -0300

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- add winbar at the top-right showing path
-vim.opt.winbar = "%=%m %f"
+-- add winbar at the top-right showing path vim.opt.winbar = "%=%m %f"
