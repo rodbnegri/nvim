388572b71aa06bbdfd53ec879b91484d4dfae637
rodolfo <rodolfobnegri@yahoo.com.br>
2024-10-23 10:28:43 -0300

    update

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,6 +2,10 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    priority = 1000,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 }
